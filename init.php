10c48c4debe247a91c720c19d81b289489a3c0cc
pligi <pligi@PSWeb>
2018-05-17 15:48:43 +0300

    Инициализация плагина.

---
+++ init.php
@@ -0,0 +1,34 @@
+<?php
+  define('WPGRABBER_CORE_VERSION', '1.0.0');
+  define('WPGRABBER_PLUGIN_INSTALL_DIR', WPGRABBER_PLUGIN_DIR.'install'.DIRECTORY_SEPARATOR);
+  define('WPGRABBER_PLUGIN_CORE_DIR', WPGRABBER_PLUGIN_DIR.'core'.DIRECTORY_SEPARATOR);
+  define('WPGRABBER_PLUGIN_TPL_DIR', WPGRABBER_PLUGIN_DIR.'tmpl'.DIRECTORY_SEPARATOR);
+
+  if (!session_id()) {
+    session_start();
+  }
+
+  function wpgIsDebug() {
+    return is_file(WPGRABBER_PLUGIN_DIR.'debug');
+  }
+
+  if (wpgIsDebug()) {
+    ini_set('display_errors', true);
+    error_reporting(E_ALL ^ E_NOTICE);
+  }
+
+  function wpgPlugin() {
+    return 'WPGPlugin';
+  }
+
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'WPGPlugin.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'WPGErrorHandler.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'WPGHelper.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'WPGTable.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'WPGTools.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'WPGWordPressDB.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'TGrabberCore.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'TGrabberWordPress.php');
+  require_once (WPGRABBER_PLUGIN_CORE_DIR.'TGrabberWPOptions.php');
+  call_user_func(array(wpgPlugin(), 'load'));
+?>
